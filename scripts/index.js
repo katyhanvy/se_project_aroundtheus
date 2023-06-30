be80200ddbd2acff2888b578df30c66337b046a7
katyhanvy <katherineahanvy@outlook.com>
2023-06-29 20:50:53 -0500

    new code

--- scripts/index.js
+++ scripts/index.js
@@ -23,9 +23,11 @@ const initialCards = [
     title: "Vanoise National Park",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
   },
+
   {
     title: "Lago di Braies",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
   },
 ];
+
 console.log(initialCards);
