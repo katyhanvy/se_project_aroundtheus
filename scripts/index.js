225610df2ec2b5f7982a28425da6199cf8100b44
katyhanvy <katherineahanvy@outlook.com>
2023-09-20 09:35:12 -0500

    new code

--- scripts/index.js
+++ scripts/index.js
@@ -31,21 +31,32 @@ const initialCards = [
 ];
 const profileEditButton = document.querySelector("#profile-edit-button");
 const profileEditModal = document.querySelector("#profile-edit-modal");
-const profileEditCloseButton = profileEditModal.querySelector(".modal__close");
+const profileCloseModalButton = document.querySelector("#profile-close-modal");
 const profileTitle = document.querySelector(".profile__title");
 const profileDescription = document.querySelector(".profile__description");
-const profileTitleInput = document.querySelector(".profile-title-input");
+const profileTitleInput = document.querySelector("#profile-title-input");
 const profileDescriptionInput = document.querySelector(
-  ".profile-description-input"
+  "#profile-description-input"
 );
+const profileEditForm = profileEditModal.querySelector(".modal__form");
+const cardTemplate = document.querySelector("#card-template");
+
+function closePopup() {
+  profileEditModal.classList.remove("modal_opened");
+}
 
 profileEditButton.addEventListener("click", () => {
   profileTitleInput.value = profileTitle.textContent;
-  profileDescription.value = profileDescription.textContent;
-
+  profileDescriptionInput.value = profileDescription.textContent;
   profileEditModal.classList.add("modal_opened");
 });
 
-profileCLoseModal.addEventListener("click", () => {
-  profileEditModal.classList.remove("modal_opened");
+profileCloseModalButton.addEventListener("click", () => {
+  closePopup();
+});
+profileEditForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  profileTitle.textContent = profileTitleInput.value;
+  profileDescription.textContent = profileDescriptionInput.value;
+  closePopup();
 });
